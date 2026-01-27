94c9adae407081a7bd38dd94a7c041ac88991c7f
elifberraketen <ketenelifberra@gmail.com>
2026-01-27 23:37:11 +0300

    start

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;                       //for the GUI
+import java.util.Dictionary;
+
+
+public class App {
+    static void main(String[] args) {
+
+        int rowNum = 21;  //from 0 to 20
+        int colubmnNum = 19 ;
+        int tileSize = 32;
+        int tableWidth = colubmnNum * tileSize;
+        int tableHeigth = rowNum * tileSize ;
+
+        JFrame frame = new JFrame("PacMan");
+        frame.setVisible(true);
+        frame.setSize(tableWidth,tableHeigth);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);      //center of our screen
+        frame.setResizable(false);
+    }
+}
